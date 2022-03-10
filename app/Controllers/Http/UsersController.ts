f1c8f86e62b88427b00f481b94fb9aa15bd1a0e8
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-03-10 11:28:04 +0700

    feat: added swagger docs

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -6,12 +6,12 @@ import User from 'App/Models/User';
 export default class UsersController {
     public async store({ request, response }: HttpContextContract) {
         const input = request.only(['username','email']);
-        
+
         try {
             const users = await User.create(input);
 
             return response.status(200).json({ code: 200, status: 'success', data: users });
-            
+
         } catch (err) {
             return response.status(500).json({ code: 500, status: 'error', message: err.message });
         }
