3f31aa9aae4c1f71289b4703f80ac17f0601d5be
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-03-10 09:45:41 +0700

    feat: added basic CRUD

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,10 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.group(() => {
+    Route.post('users', 'UsersController.store')
+    Route.get('users', 'UsersController.index')
+    Route.get('users/:id', 'UsersController.show')
+    Route.put('users/:id', 'UsersController.update')
+    Route.delete('users/:id', 'UsersController.delete')
+}).prefix('/api/v1')
